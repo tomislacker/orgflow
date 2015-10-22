1f45f214f096db590c6726f964ef23b86bed18a8
Ben Tomasik <ben@tomasik.io>
2015-10-21 20:51:58 -0500

    Add relationship beteen Team and User

--- src/orgflow/sql.py
+++ src/orgflow/sql.py
@@ -1,7 +1,9 @@
 from sqlalchemy import Column
+from sqlalchemy import ForeignKey
 from sqlalchemy import String
 from sqlalchemy import INTEGER
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
 MYSQL_CREDS = {
@@ -19,12 +21,33 @@ class Team(Base):
     __tablename__ = 'team'
     id = Column(INTEGER(unsigned=True), primary_key=True)
     name = Column(String(128))
+    users = relationship(
+        'User',
+        secondary='team_user_link'
+    )
+
+
+class TeamUserLink(Base):
+    __tablename__ = 'team_user_link'
+    team_id = Column(
+        INTEGER(unsigned=True),
+        ForeignKey('team.id'),
+        primary_key=True)
+
+    user_id = Column(
+        INTEGER(unsigned=True),
+        ForeignKey('user.id'),
+        primary_key=True)
 
 
 class User(Base):
     __tablename__ = 'user'
     id = Column(INTEGER(unsigned=True), primary_key=True)
     username = Column(String(128))
+    teams = relationship(
+        'Team',
+        secondary='team_user_link'
+    )
 
 
 from sqlalchemy import create_engine
